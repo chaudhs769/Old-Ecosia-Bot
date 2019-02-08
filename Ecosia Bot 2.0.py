e354154067d4072a72cdcf1f5ed1469f7bfdfa16
Thor <38732268+Thor0032@users.noreply.github.com>
2019-02-09 00:32:06 +0100

    Update Ecosia Bot 2.0.py

--- Ecosia Bot 2.0.py
+++ Ecosia Bot 2.0.py
@@ -12,7 +12,7 @@ def start():
         write = random.choice(list)
 
     try:
-        pyautogui.click(pyautogui.locateCenterOnScreen('Searchbar.PNG', grayscale=True, confidence=0.7))
+        pyautogui.click(pyautogui.locateCenterOnScreen('Searchbar.PNG', grayscale=True, confidence=0.6))
         pyautogui.write(Keyboard.write)
         sleep(0.1)
         pyautogui.press('enter')
@@ -25,12 +25,26 @@ def start():
         print("Success")
         sleep(10)
     except ImageNotFoundException:
-        print("Image not found")
-        sleep(1)
-        pyautogui.click(pyautogui.locateCenterOnScreen('Ecosia2.png', confidence=0.3))
-        pyautogui.moveTo(20, None)
-        sleep(3)
-        pyautogui.hotkey('ctrl', '1'), sleep(0.1), pyautogui.hotkey('ctrl', 'w')
+        try:
+            pyautogui.click(pyautogui.locateCenterOnScreen('Searchbar2.PNG', grayscale=True, confidence=0.6))
+            pyautogui.write(Keyboard.write)
+            sleep(0.1)
+            pyautogui.press('enter')
+            sleep(0.1)
+            pyautogui.click(pyautogui.locateCenterOnScreen('Ecosia2.png', confidence=0.3))
+            sleep(0.1)
+            pyautogui.move(20, None)
+            sleep(0.2)
+            pyautogui.hotkey('ctrl', '1'), sleep(0.5), pyautogui.hotkey('ctrl', 'w')
+            print("Success")
+            sleep(10)
+        except ImageNotFoundException:
+            sleep(1)
+            print("Image not found")
+            pyautogui.click(pyautogui.locateCenterOnScreen('Ecosia2.png', confidence=0.3))
+            pyautogui.moveTo(20, None)
+            sleep(3)
+            pyautogui.hotkey('ctrl', '1'), sleep(0.1), pyautogui.hotkey('ctrl', 'w')
 
 
 while True:

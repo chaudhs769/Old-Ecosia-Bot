ff5e0f1bee47b042c931c70bac4e1fb531d3aaa3
Thor <thorblanke@gmail.com>
2019-02-08 20:30:19 +0100

    Initial commit

---
+++ Ecosia Bot 2.0.py
@@ -0,0 +1,38 @@
+# Author Thor
+import pyautogui
+from time import sleep
+import random
+from pyscreeze import ImageNotFoundException
+
+
+def start():
+    class Keyboard:
+        list = ["Lars og Henrik", "Er aber seje?", "Hvem er Kaj Andersen?", "Jeg elsker at hjælpe til med miljøet :)",
+                "Vom", "Overlund Skole"]
+        write = random.choice(list)
+
+    try:
+        pyautogui.click(pyautogui.locateCenterOnScreen('Searchbar.PNG', grayscale=True, confidence=0.7))
+        pyautogui.write(Keyboard.write)
+        sleep(0.1)
+        pyautogui.press('enter')
+        sleep(0.1)
+        pyautogui.click(pyautogui.locateCenterOnScreen('Ecosia2.png', confidence=0.3))
+        sleep(0.1)
+        pyautogui.move(20, None)
+        sleep(0.2)
+        pyautogui.hotkey('ctrl', '1'), sleep(0.5), pyautogui.hotkey('ctrl', 'w')
+        print("Success")
+        sleep(10)
+    except ImageNotFoundException:
+        print("Image not found")
+        sleep(1)
+        pyautogui.click(pyautogui.locateCenterOnScreen('Ecosia2.png', confidence=0.3))
+        pyautogui.moveTo(20, None)
+        sleep(3)
+        pyautogui.hotkey('ctrl', '1'), sleep(0.1), pyautogui.hotkey('ctrl', 'w')
+
+
+while True:
+    sleep(1)
+    start()

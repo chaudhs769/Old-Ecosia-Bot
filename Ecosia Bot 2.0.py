6b0ccc1dc47ad61a6efd4821208a263c0a811b11
Thor <38732268+Thor0032@users.noreply.github.com>
2019-02-17 02:09:43 +0100

    Updated; Details and a few changes.
    I've updated the word-list, changed some movements from the x-axis to the y-axis, and I've added some text between some lines, to make the code less confusing for people that are not used to pyautogui.

--- Ecosia Bot 2.0.py
+++ Ecosia Bot 2.0.py
@@ -7,25 +7,36 @@ from pyscreeze import ImageNotFoundException
 
 def start():
     class Keyboard:
-        list = ["Lars og Henrik", "Er aber seje?", "Hvem er Kaj Andersen?", "Jeg elsker at hjælpe til med miljøet :)",
-                "Vom", "Overlund Skole"]
+        # The list where a word gets chosen for later.
+        list = ["Lars and Henrik", "Are monkeys cool?", "Who is Kaj Andersen?", "It's always nice help our climate!",
+                "Yeet", "United States America", "So ehmm Denmark? Great..", "GuEsS thEy nEvEr miSS hUh?"]
+        # Picks a random word from the list.
         write = random.choice(list)
 
     try:
+        # Try and click the 'Searchbar.PNG if found, with grayscale on to make it faster, and a confidence at 0.6 because that's a good
+        # value that works very well. Confidence is just how confident the bot is that it's 100% that image it's looking for.
         pyautogui.click(pyautogui.locateCenterOnScreen('Searchbar.PNG', grayscale=True, confidence=0.6))
+        # Writes the random word
         pyautogui.write(Keyboard.write)
         sleep(0.1)
         pyautogui.press('enter')
         sleep(0.1)
         pyautogui.click(pyautogui.locateCenterOnScreen('Ecosia2.png', confidence=0.3))
         sleep(0.1)
-        pyautogui.move(20, None)
+        # Moves the cursor 20 pixels down the Y-axis, so the Ecosia picture can be used again without confusing the bot.
+        pyautogui.move(None, 20)
         sleep(0.2)
+        # Jumps to the old tab and closes it.
         pyautogui.hotkey('ctrl', '1'), sleep(0.5), pyautogui.hotkey('ctrl', 'w')
+        # And finally prints success if it succeeded.
         print("Success")
         sleep(10)
+    # And if it did not, it tries this:
     except ImageNotFoundException:
+        print("Image not found, trying another one..")
         try:
+            # Tries with another picture from the library, but still the same confidence.
             pyautogui.click(pyautogui.locateCenterOnScreen('Searchbar2.PNG', grayscale=True, confidence=0.6))
             pyautogui.write(Keyboard.write)
             sleep(0.1)
@@ -33,20 +44,21 @@ def start():
             sleep(0.1)
             pyautogui.click(pyautogui.locateCenterOnScreen('Ecosia2.png', confidence=0.3))
             sleep(0.1)
-            pyautogui.move(20, None)
+            pyautogui.move(None, 20)
             sleep(0.2)
             pyautogui.hotkey('ctrl', '1'), sleep(0.5), pyautogui.hotkey('ctrl', 'w')
             print("Success")
             sleep(10)
         except ImageNotFoundException:
             sleep(1)
-            print("Image not found")
+            print("Image not found, most likely caused by a server error or timeout.")
             pyautogui.click(pyautogui.locateCenterOnScreen('Ecosia2.png', confidence=0.3))
-            pyautogui.moveTo(20, None)
+            pyautogui.moveTo(None, 20)
             sleep(3)
-            pyautogui.hotkey('ctrl', '1'), sleep(0.1), pyautogui.hotkey('ctrl', 'w')
+            pyautogui.hotkey('ctrl', '1'), sleep(0.5), pyautogui.hotkey('ctrl', 'w')
 
 
 while True:
+    # Gives you one second from when opening the terminal, to the bot starts doing its magic, and also gives it a little cooldown.
     sleep(1)
     start()
